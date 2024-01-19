43c75d5bdee247792240ad6455809e77dd74b00d
Jared <jsmcreynolds@charter.net>
2024-01-19 16:56:37 -0500

    Adding timeline feature to the About Us section

---
+++ src/components/timeContent.js
@@ -0,0 +1,13 @@
+const TimeContent = (props) => {
+
+    return (
+        <>
+            <div className="timeContent">
+                <h2>{props.date}</h2>
+            </div>
+        </>
+        
+    )
+}
+
+export default TimeContent;
--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,6 +1,59 @@
+import TimeContent from "../components/timeContent";
 
-const About = () => {
-
+const About = (props) => {
+    const date1 = "2008"
+    return (
+        <div className="main-band">
+            <div className="timeline">
+                <div className="timeRow row1">
+                    <div className="blank blank1"></div>
+                    <div className="timeBox timeContainer1">
+                        <TimeContent date= {date1}/>
+                    </div>
+                </div>
+                <div className="spacer">
+                    <div className="spacer1"></div>
+                    <div className="spacer2"></div>
+                    <div className="spacer3"></div>
+                </div>
+                <div className="timeRow row2">
+                    <div className="timeBox timeContainer2">
+                        <TimeContent date= {date1}/>
+                    </div>
+                    <div className="blank blank2"></div>
+                </div>
+                <div className="spacer"></div>
+                <div className="timeRow row3">
+                    <div className="blank blank3"></div>
+                    <div className="timeBox timeContainer3">
+                        <TimeContent date= {date1}/>
+                    </div>
+                </div>
+                <div className="spacer"></div>
+                <div className="timeRow row4">
+                    <div className="timeBox timeContainer4">
+                        <TimeContent date= {date1}/>
+                    </div>
+                    <div className="blank blank4"></div>
+                </div>
+                <div className="spacer"></div>
+                <div className="timeRow row5">
+                    <div className="blank blank1"></div>
+                    <div className="timeBox timeContainer5">
+                        <TimeContent date= {date1}/>
+                    </div>
+                </div>
+                <div className="spacer"></div>
+                <div className="timeRow row6">
+                    <div className="timeBox timeContainer6">
+                        <TimeContent date= {date1}/>
+                    </div>
+                    <div className="blank blank5"></div>
+                </div>
+            </div>
+        </div>
+        
+    )
 }
 
 export default About;
